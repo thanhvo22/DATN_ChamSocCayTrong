f186f4cc05ee483e25a20f764b59b85f17f679f6
Thành <thanhvp2707@gmail.com>
2022-03-10 10:18:29 +0700

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -3,32 +3,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 
-const dbConnect = async () => {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://thanhvo22:2212@chamsoccaytrong-v1.owuep.mongodb.net/chamsoccaytrong_v1?retryWrites=true&w=majority`
-    );
+// const dbConnect = async () => {
+//   try {
+//     await mongoose.connect(
+//       `mongodb+srv://thanhvo22:2212@chamsoccaytrong-v1.owuep.mongodb.net/chamsoccaytrong_v1?retryWrites=true&w=majority`
+//     );
 
-    console.log("mongodb connected!");
+//     console.log("mongodb connected!");
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// };
 
-  } catch (error) {
-    console.log(error.message);
-    process.exit(1);
-  }
-};
-
-//const db = require('./connect');
+const db = require('./connect');
 
 const app = express();
-// db.connect();
-dbConnect();
+db.connect();
+//dbConnect();
 
 app.use(express.json());
 //app.use(cors());
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-
-
 
 app.get("/", (req, res) => {
   res.send("Đồ án tốt nghiệp !");
