8d9f15ea687e3b2d4928f1d476ba3e3f10535de7
Thanh_Vo <thanhvp2707@gmail.com>
2022-03-16 10:35:08 +0700

    add model saved list

---
+++ server/models/savedList.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const savedListSchema = new Schema({
+  userID: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+  },
+  playlistID: {
+    type: Schema.Types.ObjectId,
+    ref: "Playlists"
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Savedlists", savedListSchema);
