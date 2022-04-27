c295ca5dcc858b9278fe5b7e5da387068e9e7f77
Thanh_Vo <thanhvp2707@gmail.com>
2022-04-27 21:25:24 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const db = require('./connect');
-const authRoute = require('./routes/auth.route');
+const db = require("./connect");
+const cookieMiddleWare = require("./middlewares/cookie.middleware");
+const authRoute = require("./routes/auth.route");
+const userRoute = require("./routes/user.route");
+const categoryRoute = require("./routes/category.route");
 
 const app = express();
 db.connect();
 //dbConnect();
 
 app.use(express.json());
-//app.use(cors());
+app.use(cookieParser(process.env.SESSION_SECRET)); //using signed \\ signedCookies
+// khong khac gi cookie nhung co them secret, co the ktra cookie.
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-
 
 app.get("/", (req, res) => {
   res.send("Đồ án tốt nghiệp !");
 });
-app.use('/api/v1/auth', authRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/user", cookieMiddleWare, userRoute);
+app.use("/api/v1/category", cookieMiddleWare, categoryRoute);
 
 app.listen(5000, () => {
   console.log(`Example app listening on http://localhost:5000`);
