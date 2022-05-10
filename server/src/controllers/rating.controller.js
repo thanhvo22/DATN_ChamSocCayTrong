b7ee3fa4f4b069b5958bd72334aa41c599a8c225
Thanh_Vo <thanhvp2707@gmail.com>
2022-05-10 21:58:26 +0700

    add rate

---
+++ server/src/controllers/rating.controller.js
@@ -0,0 +1,28 @@
+const rateModel = require('../models/rating.model');
+
+module.exports.getRates = async (req, res) => {
+    const rates = await rateModel.find();
+    res.json(rates);
+}
+
+module.exports.getRateForPlayList = async (req, res) => {
+    const playlist_ID = req.signedCookies.playlist_id;
+    const rateForPlaylist = await rateModel.findOne({id:playlist_ID})
+    res.json(rateForPlaylist);
+}
+
+module.exports.postCreateRate = async (req, res) => {
+    const playlist_ID = req.signedCookies.playlist_id;
+    const userID =req.singedCookies.cookie_id;
+    const {rating} = req.body;
+    const rate = rateModel.create({
+        playlist_ID,
+        userID,
+        rating
+    })
+    res.json({
+        message: 'create rate successfully',
+        rate
+    });
+
+}
